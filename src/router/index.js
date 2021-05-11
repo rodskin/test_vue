dab9a0fb2eb66efe401d580ccb648267192fa9da
fgilles <fgilles@corp.free.fr>
2021-05-11 12:12:11 +0200

    ajout page produits

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue' 
+import Products from '../views/Products.vue' 
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/products',
+        name: 'Products',
+        component: Products
+    }
 ]
 
 const router = createRouter({
---
+++ src/views/Products.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="products">
+    <h1>Liste des produits</h1>
+  </div>
+</template>
